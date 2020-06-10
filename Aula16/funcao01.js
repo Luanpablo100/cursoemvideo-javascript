7e45100a3c8ef6db9d161b3ab7840579a096a131
Luanpablo100 <lpablo611@gmail.com>
2020-06-09 20:48:23 -0400

    Aula16

---
+++ Aula16/funcao01.js
@@ -0,0 +1,11 @@
+function parImpar(n) { // Recebimento do parâmtro = Parâmetro Formal
+    if (n%2==0) {
+        return "Par!" //Resposta a chamada da função
+    } else {
+        return "Impar!"
+    }
+}
+
+console.log(parImpar(4))
+//Chamada da função + entre parênteses = Parâmetro real
+
---
+++ Aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) { // =0 são paramêtros pré-definidos para caso um dos parâmetros não sejam passados
+    return n1 + n2
+}
+
+console.log(soma(2))
